6a7f326e6a9f3a56f499ab46a92cf2aa47c3593c
NickAtNight11 <nwatson7777@gmail.com>
2022-05-30 14:32:05 -0500

    Improved Image Loading

--- src/components/CreationBox.js
+++ src/components/CreationBox.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 import './CreationBox.css'
 
 function CreationBox({image}, {imagename}) {
   return (
     <div id='creation-box'>
-        <img id='creation-image' src={image} alt={imagename}/>
+        <LazyLoadImage id='creation-image' src={image} alt={imagename} effect="black-and-white"/>
     </div>
   )
 }
