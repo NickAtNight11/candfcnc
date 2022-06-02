31e7d5b759cf7dd3d30d43dba24e23477ee4343e
NickAtNight11 <nwatson7777@gmail.com>
2022-06-01 21:04:39 -0500

    Added Email Services

--- src/components/ContactBox.js
+++ src/components/ContactBox.js
@@ -1,30 +1,57 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import emailjs from '@emailjs/browser';
 import './ContactBox.css'
 
 
 function ContactBox() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_vv2di2t', 'c&f_contact_form', form.current, 'IM4DbB_TEKpV6HsXa')
+    .then((result) => {
+      console.log(result.text);
+  }, (error) => {
+      console.log(error.text);
+  });
+  };
+
+
   return (
     <div id="contact-box">
-        <h2>Contact Information</h2>
-        <div id="contact-box-grid">
-            <h3><i>Mailing Address:</i></h3>
-            <h3>C & F CNC<br/>
+      <h2>Contact Us</h2>
+      <div id="contact-box-inner">
+        <form ref={form} onSubmit={sendEmail} id="contact-box-form">
+          <label>Name *</label>
+          <input type="text" name="from_name" className="form-credential" required/>
+          <label>Email *</label>
+          <input type="email" name="user_email" className="form-credential" required/>
+          <label>Subject</label>
+          <input type="text" name="subject" className="form-credential" />
+          <label>Message</label>
+          <textarea name="message" id="form-message"/>
+          <input type="submit" value="Send" id="form-send"/>
+        </form>
+        <div id="contact-box-seperator"/>
+        <div id="contact-box-info">
+            <h4><i>Mailing Address:</i><br/>
+                C & F CNC<br/>
                 PO Box 485<br/>
                 Amherst, TX 79312
-            </h3>
-            <div/>
-            <h3><i>Email:</i></h3>
-            <h3>thompsonc62@aol.com</h3>
-            <div/>
-            <h3><i>Phone:</i></h3>
-            <h3>806-246-3838 (Business)</h3>
-            <h3>806-241-6865 (Cell)<br/>
+            </h4>
+            <h4><i>Email:</i><br/>
+                thompsonc62@aol.com
+            </h4>
+            <h4><i>Phone:</i><br/>
+                806-246-3838 (Business)<br/>
+                806-241-6865 (Cell)<br/>
                 806-441-4294 (Cell)
-            </h3>
-            <h3><i>Facebook:</i></h3>
-            <h3>C & F CNC Creations</h3>
-            <div/>
+            </h4>
+            <h4><i>Facebook:</i><br/>
+                C & F CNC Creations</h4>
         </div>
+      </div>
     </div>
   )
 }
