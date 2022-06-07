dbd2128c7fcd7ae95443c3d43dfaef0e75508636
NickAtNight11 <nwatson7777@gmail.com>
2022-06-07 12:32:08 -0500

    Added Admin Page

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -14,9 +14,9 @@ const firebaseConfig = {
   projectId: `${process.env.REACT_APP_FIREBASE_PROJECTID}`,
   storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGEBUCKET}`,
   messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID}`,
-  appId: `${process.env.REACT_APP_FIREBASE_APPID}`
-};
-
+  appId: `${process.env.REACT_APP_FIREBASE_APPID}`,
+  measurementId: `${process.env.REACT_APP_FIREBASE_MEASUREMENTID}`
+}
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
